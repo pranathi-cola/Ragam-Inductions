745f8d522bf132b905033fef4cb7bf7c9a374f91
pranathi-cola <165368133+pranathi-cola@users.noreply.github.com>
2024-12-09 11:50:57 +0530

    Making it responsive

--- src/App.js
+++ src/App.js
@@ -5,22 +5,36 @@ import jeremy from './image-jeremy.png'
 
 console.log(data);
 
-function Bro({updateTemplate})
+function Bro({updateTemplate, screenon})
 {
+  const myFun = (status) => {
+    if(screenon !== status)
+    {
+      return {
+        color: "hsl(236, 100%, 87%)"
+      }
+    }
+    else
+    {
+      return {
+        color: "white"
+      }
+    }
+  }
   return (
     <div style={{backgroundColor: "hsl(235, 46%, 20%)"}} className='bro-template'>
       <div style={{backgroundColor: "hsl(246, 80%, 60%)"}} className='bro-info'>
         <div className='jerry-img'><img src={jeremy}></img></div>
         <div className='jerry-info'>
-          <p>Report for</p>
-          <p>Jeremy Robson</p>
+          <p className='report-for'>Report for</p>
+          <p className='name'>Jeremy Robson</p>
         </div>
       </div>
       <div>
         <ul className='calender-info'>
-          <li onClick={() => updateTemplate('daily')}>Daily</li>
-          <li onClick={() => updateTemplate('weekly')}>Weekly</li>
-          <li onClick={() => updateTemplate('monthly')}>Monthly</li>
+          <li style={myFun('daily')} onClick={() => updateTemplate('daily')}>Daily</li>
+          <li style={myFun('weekly')} onClick={() => updateTemplate('weekly')}>Weekly</li>
+          <li style={myFun('monthly')} onClick={() => updateTemplate('monthly')}>Monthly</li>
         </ul>
       </div>
     </div>
@@ -39,7 +53,7 @@ function Template_Info({todo, current, previous, color, svgData})
       <div style={{backgroundColor: "hsl(235, 46%, 20%)"}} className='info-in-template'>
         <div className='header'>
           <p className='todo'>{todo}</p>
-          <p>...</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="21" height="5"><path xmlns="http://www.w3.org/2000/svg" d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
         </div>
         <div className='current'>{current}hrs</div>
         <div className='previous'>Last Week - {previous}hrs</div>
@@ -55,7 +69,7 @@ function App() {
   }
   return (
     <div style={{backgroundColor: "hsl(226, 43%, 10%)"}} className='main-window'>
-      <Bro updateTemplate={updateTemplate}/>
+      <Bro updateTemplate={updateTemplate} screenon={screenon}/>
       <div className='templates-collection'>
       {data.map((item) => (
           <Template_Info
