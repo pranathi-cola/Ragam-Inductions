dc1dff6208dca09e1ab1458dda7bab63205bd541
pranathi-cola <165368133+pranathi-cola@users.noreply.github.com>
2024-12-09 15:11:43 +0530

    Deploy Website

--- src/App.js
+++ src/App.js
@@ -55,8 +55,10 @@ function Template_Info({todo, current, previous, color, svgData})
           <p className='todo'>{todo}</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="21" height="5"><path xmlns="http://www.w3.org/2000/svg" d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
         </div>
-        <div className='current'>{current}hrs</div>
-        <div className='previous'>Last Week - {previous}hrs</div>
+        <div className='data'>
+          <div className='current'>{current}hrs</div>
+          <div className='previous'>Last Week - {previous}hrs</div>
+        </div>
       </div>
     </div>
   )
